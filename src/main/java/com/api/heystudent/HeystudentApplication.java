1bcd298e040267c6dc921fedc1d0b56dd4d96e70
Abdul Rahuman <abdul.rahuman@athleticgreens.com>
2025-02-16 16:06:29 -0500

    My first commit

---
+++ src/main/java/com/api/heystudent/HeystudentApplication.java
@@ -0,0 +1,13 @@
+package com.api.heystudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeystudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeystudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/heystudent/HeystudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.heystudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeystudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
